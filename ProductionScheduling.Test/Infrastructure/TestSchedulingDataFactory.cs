c8928944aa69a92bc6154a130413da252ceb1745
neetall <794160766@qq.com>
2026-07-16 22:04:33 +0800

    feat: 添加测试类

--- ProductionScheduling.Test/Infrastructure/TestSchedulingDataFactory.cs
+++ ProductionScheduling.Test/Infrastructure/TestSchedulingDataFactory.cs
@@ -120,4 +120,342 @@ public static class TestSchedulingDataFactory
 
         return context;
     }
+    
+    public static SchedulingContext CreateGreedySchedulerContext()
+{
+    var order =
+        new Order
+        {
+            Code = "ORD001",
+            Priority = 1,
+            DueDate =
+                DateTime.Today.AddDays(1)
+        };
+
+
+    order.JobTickets.AddRange(
+    [
+        new JobTicket
+        {
+            Code = "JT001",
+            Sequence = 1,
+            Length = 100
+        },
+
+
+        new JobTicket
+        {
+            Code = "JT002",
+            Sequence = 2,
+            Length = 200
+        }
+    ]);
+
+
+
+    var machines =
+        new List<Machine>
+        {
+            new()
+            {
+                Code = "M001",
+
+                Capabilities =
+                [
+                    new MachineCapability
+                    {
+                        MachineCode = "M001",
+                        JobTicketCode = "JT001",
+                        HourlyCapacity = 50,
+                        SetupMinutes = 0
+                    },
+
+                    new MachineCapability
+                    {
+                        MachineCode = "M001",
+                        JobTicketCode = "JT002",
+                        HourlyCapacity = 50,
+                        SetupMinutes = 0
+                    }
+                ]
+            },
+
+
+            new()
+            {
+                Code = "M002",
+
+                Capabilities =
+                [
+                    new MachineCapability
+                    {
+                        MachineCode = "M002",
+                        JobTicketCode = "JT001",
+                        HourlyCapacity = 100,
+                        SetupMinutes = 0
+                    }
+                ]
+            }
+        };
+
+
+
+    var context =
+        new SchedulingContext
+        {
+            Orders =
+            [
+                order
+            ],
+
+            Machines =
+                machines,
+
+            Options =
+            {
+                TimeGranularityMinutes = 60
+            }
+        };
+
+
+
+    context.FactoryCalendars.Add(
+        new FactoryCalendar
+        {
+            FactoryCode = "F001",
+
+            Periods =
+            [
+                new ShiftPeriod
+                {
+                    StartTime =
+                        DateTime.Today.AddHours(8),
+
+                    EndTime =
+                        DateTime.Today.AddHours(18)
+                }
+            ]
+        });
+
+
+    return context;
+}
+    
+    public static SchedulingContext CreateMultiOrderContext()
+{
+    var order1 =
+        new Order
+        {
+            Code = "ORD001",
+            Priority = 1,
+            DueDate =
+                DateTime.Today.AddDays(1)
+        };
+
+
+    order1.JobTickets.Add(
+        new JobTicket
+        {
+            Code = "JT001",
+            Sequence = 1,
+            Length = 100
+        });
+
+
+
+    var order2 =
+        new Order
+        {
+            Code = "ORD002",
+            Priority = 2,
+            DueDate =
+                DateTime.Today.AddDays(1)
+        };
+
+
+    order2.JobTickets.Add(
+        new JobTicket
+        {
+            Code = "JT002",
+            Sequence = 1,
+            Length = 100
+        });
+
+
+
+    var machines =
+        new List<Machine>
+        {
+            new()
+            {
+                Code = "M001",
+
+                Capabilities =
+                [
+                    new MachineCapability
+                    {
+                        MachineCode = "M001",
+                        JobTicketCode = "JT001",
+                        HourlyCapacity = 100,
+                        SetupMinutes = 0
+                    },
+
+                    new MachineCapability
+                    {
+                        MachineCode = "M001",
+                        JobTicketCode = "JT002",
+                        HourlyCapacity = 100,
+                        SetupMinutes = 0
+                    }
+                ]
+            }
+        };
+
+
+
+    var context =
+        new SchedulingContext
+        {
+            Orders =
+            [
+                order1,
+                order2
+            ],
+
+            Machines =
+                machines,
+
+            Options =
+            {
+                TimeGranularityMinutes = 60
+            }
+        };
+
+
+
+    context.FactoryCalendars.Add(
+        new FactoryCalendar
+        {
+            FactoryCode = "F001",
+
+            Periods =
+            [
+                new ShiftPeriod
+                {
+                    StartTime =
+                        DateTime.Today.AddHours(8),
+
+                    EndTime =
+                        DateTime.Today.AddHours(18)
+                }
+            ]
+        });
+
+
+
+    return context;
+}
+    
+    public static SchedulingContext CreateMachineConflictContext()
+    {
+        var order =
+            new Order
+            {
+                Code = "ORD001",
+                Priority = 1
+            };
+
+
+        order.JobTickets.AddRange(
+        [
+            new JobTicket
+            {
+                Code = "JT001",
+                Sequence = 1,
+                Length = 200
+            },
+
+
+            new JobTicket
+            {
+                Code = "JT002",
+                Sequence = 2,
+                Length = 200
+            }
+        ]);
+
+
+
+        var machines =
+            new List<Machine>
+            {
+                new()
+                {
+                    Code = "M001",
+
+                    Capabilities =
+                    [
+                        new MachineCapability
+                        {
+                            MachineCode = "M001",
+                            JobTicketCode = "JT001",
+                            HourlyCapacity = 100,
+                            SetupMinutes = 0
+                        },
+
+
+                        new MachineCapability
+                        {
+                            MachineCode = "M001",
+                            JobTicketCode = "JT002",
+                            HourlyCapacity = 100,
+                            SetupMinutes = 0
+                        }
+                    ]
+                }
+            };
+
+
+
+        var context =
+            new SchedulingContext
+            {
+                Orders =
+                [
+                    order
+                ],
+
+                Machines =
+                    machines,
+
+                Options =
+                {
+                    TimeGranularityMinutes = 60
+                }
+            };
+
+
+
+        context.FactoryCalendars.Add(
+            new FactoryCalendar
+            {
+                FactoryCode = "F001",
+
+                Periods =
+                [
+                    new ShiftPeriod
+                    {
+                        StartTime =
+                            DateTime.Today.AddHours(8),
+
+                        EndTime =
+                            DateTime.Today.AddHours(18)
+                    }
+                ]
+            });
+
+
+
+        return context;
+    }
 }
--- ProductionScheduling.Test/obj/Debug/net10.0/ProductionScheduling.Test.AssemblyInfo.cs
+++ ProductionScheduling.Test/obj/Debug/net10.0/ProductionScheduling.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductionScheduling.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4b7f1601e65c3691ed83bee54bca995aa00657")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714c6f44ab47dee80e53168abd005e04b83ae54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductionScheduling.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductionScheduling.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
